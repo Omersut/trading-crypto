e6ffb4f032cb755ab6306791361710039bfbd987
Omer Sut <omersutcom@gmail.com>
2022-03-04 21:55:16 +0300

    fixcontinues

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
 import "./App.css";
 import List from "./components/List";
-import Wallet from "./components/Wallet";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [loggedin, setIsloggedin] = useState(
+    localStorage.getItem("loggedin") ? "true" : "false"
+  );
+
+  const Submit = async () => {
+    setIsloggedin("true");
+
+    localStorage.setItem("loggedin", JSON.stringify(loggedin));
+  };
+
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<List />} />
-          <Route path="/wallet" element={<Wallet />} />
-        </Routes>
-      </Router>
+      {loggedin == "false" ? (
+        <div style={{ marginTop: "230px" }} className="form">
+          <div className="subtitle">Welcome 👋 Let's create your account!</div>
+          <div className="input-container ic1">
+            <input
+              style={{ marginTop: "10px" }}
+              id="username"
+              type="text"
+              placeholder="username"
+            />
+            <button style={{ marginLeft: "2px" }} onClick={Submit} type="text">
+              Submit
+            </button>
+          </div>
+        </div>
+      ) : (
+        <Router>
+          <Routes>
+            <Route path="/" element={<List />} />
+          </Routes>
+        </Router>
+      )}
     </div>
   );
 }
